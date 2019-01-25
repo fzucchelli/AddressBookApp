af47047caea382d44b32269be9984cc103463b96
fzucchelli <frankzucche@gmail.com>
2019-01-25 21:06:38 +0500

    deployment

---
+++ precache-manifest.07cb511d91adb2731ef98f3390145a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7dd78bae4e070f003e1",
+    "url": "/static/js/main.d7dd78ba.chunk.js"
+  },
+  {
+    "revision": "5515615773aa1587387c",
+    "url": "/static/js/1.55156157.chunk.js"
+  },
+  {
+    "revision": "d7dd78bae4e070f003e1",
+    "url": "/static/css/main.27728ebd.chunk.css"
+  },
+  {
+    "revision": "5515615773aa1587387c",
+    "url": "/static/css/1.81c181be.chunk.css"
+  },
+  {
+    "revision": "3f449d3be839497f3ad9fea409330503",
+    "url": "/index.html"
+  }
+];
